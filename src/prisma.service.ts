5b5ed97648b36c365df45fed7b581760168e7b48
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-13 13:58:23 -0300

    :wrench: Building Data Base tables and making validations

---
+++ src/prisma.service.ts
@@ -0,0 +1,17 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    /* executa algo assim que a aplicacao rodar, aqui ele conecta com o 
+    prisma assim que a aplicacao rode*/
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
